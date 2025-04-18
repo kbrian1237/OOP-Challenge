92664949809ab547b298ec13809274868c24d094
kbrian1237 <156768383+kbrian1237@users.noreply.github.com>
2025-04-18 10:34:16 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Create pet
+pet = Pet('Max')
+
+# Perform actions
+pet.eat()
+pet.play()
+pet.sleep()
+
+# Train pet
+pet.train('roll over')
+pet.train('play dead')
+pet.train('sit')
+
+# pet's status
+pet.get_status()
